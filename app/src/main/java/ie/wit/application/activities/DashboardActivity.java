5117bca4a3296be3396218d4d3e027e8d7d03a9f
Joe Wemyss <joewemyss3@gmail.com>
2017-04-09 10:23:26 +0100

    fixes #16

--- app/src/main/java/ie/wit/application/activities/DashboardActivity.java
+++ app/src/main/java/ie/wit/application/activities/DashboardActivity.java
@@ -1,10 +1,8 @@
 package ie.wit.application.activities;
 
 import android.os.Bundle;
-import android.support.annotation.IdRes;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
