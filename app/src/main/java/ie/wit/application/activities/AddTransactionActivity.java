db0a0dc8d2374aaf804ffe593267017f2445a62e
Joe Wemyss <joewemyss3@gmail.com>
2017-04-09 12:36:41 +0100

    wired up AddTransactionService

--- app/src/main/java/ie/wit/application/activities/AddTransactionActivity.java
+++ app/src/main/java/ie/wit/application/activities/AddTransactionActivity.java
@@ -7,8 +7,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import ie.wit.application.R;
 import ie.wit.application.model.Transaction;
+import ie.wit.application.service.AddTransactionService;
 
 /**
  * This class will be the activity for adding a transaction to the associated transactions for a specific user
@@ -21,6 +25,11 @@ public class AddTransactionActivity extends InternalActivity
 
     private Long currentTimestamp;
 
+    private static final Map<String, EditText> fields = new HashMap<>(2);
+    private static final Map<String, String> transactionDetails = new HashMap<>(3);
+
+    private AddTransactionService addTransactionService;
+
     /**
      * {{@inheritDoc}}
      *
@@ -32,6 +41,7 @@ public class AddTransactionActivity extends InternalActivity
         super.onCreate(savedInstanceState);
         Bundle extras = getIntent().getExtras();
         setContentView(R.layout.activity_add_income);
+        addTransactionService = new AddTransactionService(this);
         title = (EditText) findViewById(R.id.addIncomeTitle);
         amount = (EditText) findViewById(R.id.addIncomeAmount);
         if (extras != null) {
@@ -49,6 +59,13 @@ public class AddTransactionActivity extends InternalActivity
         }
     }
 
+    private void setUpReferences(){
+        fields.put("title", ((EditText)findViewById(R.id.addIncomeTitle)));
+        fields.put("amount", ((EditText)findViewById(R.id.addIncomeAmount)));
+    }
+    private void validateAndCreate(){
+
+    }
     /**
      * Create the transaction and add it to the list.
      *
