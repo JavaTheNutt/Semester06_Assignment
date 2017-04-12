ca6f13dc82052c1cf8703d1d9884e3f0c796c54d
Joe Wemyss <joewemyss3@gmail.com>
2017-04-12 11:56:09 +0100

    removed extras from dashboard

--- app/src/main/java/ie/wit/application/activities/DashboardActivity.java
+++ app/src/main/java/ie/wit/application/activities/DashboardActivity.java
@@ -32,15 +32,9 @@ import ie.wit.application.model.ui.UserDisplayNameObserver;
  */
 public class DashboardActivity extends InternalActivity
 {
-    private ListView listView;
-    private TextView noDataFoundLabel;
-
-    private ArrayAdapter<Transaction> transactionAdapter;
-
     private BalanceObserver observer;
     private UserDisplayNameObserver userObserver;
 
-    private boolean listShown = false;
     private boolean totalsShown = true;
 
     /**
@@ -70,94 +64,8 @@ public class DashboardActivity extends InternalActivity
         }
         transactionDataService.registerBalanceObserver(observer);
         authService.registerUserObserver(userObserver);
-        transactionDataService.registerTransactionCallback(result -> updateView());
-        transactionAdapter = new TransactionAdapter(this, transactionDataService.getTransactions());
-        listView.setAdapter(transactionAdapter);
-        registerForContextMenu(listView);
     }
 
-    /**
-     * Called when a context menu for the {@code view} is about to be shown.
-     * Unlike onCreateOptionsMenu(Menu), this will be called every
-     * time the context menu is about to be shown and should be populated for
-     * the view (or item inside the view for {@link AdapterView} subclasses,
-     * this can be found in the {@code menuInfo})).
-     * <p>
-     * Use {@link #onContextItemSelected(MenuItem)} to know when an
-     * item has been selected.
-     * <p>
-     * It is not safe to hold onto the context menu after this method returns.
-     *
-     * @param menu     the context menu
-     * @param v        the view the user selected
-     * @param menuInfo info about the item selected
-     */
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
-    {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.dashboard_list_context_menu, menu);
-    }
-
-    /**
-     * This hook is called whenever an item in a context menu is selected. The
-     * default implementation simply returns false to have the normal processing
-     * happen (calling the item's Runnable or sending a message to its Handler
-     * as appropriate). You can use this method for any items for which you
-     * would like to do processing without those other facilities.
-     * <p>
-     * Use {@link MenuItem#getMenuInfo()} to get extra information set by the
-     * View that added this menu item.
-     * <p>
-     * Derived classes should call through to the base class for it to perform
-     * the default menu handling.
-     *
-     * @param item The context menu item that was selected.
-     * @return boolean Return false to allow normal context menu processing to
-     * proceed, true to consume it here.
-     */
-    @Override
-    public boolean onContextItemSelected(MenuItem item)
-    {
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        switch (item.getItemId()) {
-            case R.id.contextEditItem:
-                Log.d(TAG, "onContextItemSelected: edit selected");
-                manipulateItem(info.position, true);
-                return true;
-            case R.id.contextDeleteItem:
-                Log.d(TAG, "onContextItemSelected: delete selected");
-                manipulateItem(info.position, false);
-                return true;
-        }
-        return super.onContextItemSelected(item);
-    }
-
-    private void manipulateItem(int position, boolean isEdit)
-    {
-        Log.d(TAG, "manipulateItem: manipulating item at position" + position);
-        Transaction transaction = (Transaction) listView.getItemAtPosition(position);
-        if (isEdit) {
-            editItem(transaction);
-            return;
-        }
-        deleteItem(transaction);
-    }
-
-    private void deleteItem(Transaction transaction)
-    {
-        Log.d(TAG, "deleteItem: item to be deleted: " + transaction.toString());
-        transactionDataService.removeTransaction(transaction.getTimestamp());
-    }
-
-    private void editItem(Transaction transaction)
-    {
-        Log.d(TAG, "editItem: editing transaction: " + transaction.toString());
-        Intent intent = new Intent(this, AddTransactionActivity.class);
-        intent.putExtra("transaction", transaction);
-        startActivity(intent);
-    }
 
     /**
      * {@inheritDoc}
@@ -170,21 +78,7 @@ public class DashboardActivity extends InternalActivity
         transactionDataService.stop();
     }
 
-    /**
-     * Toggle list.
-     *
-     * @param v the v
-     */
-    public void toggleList(View v)
-    {
-        listShown = !listShown;
-        int visibilityId = listShown ? View.VISIBLE : View.GONE;
-        int buttonTextId = listShown ? R.string.dashboardHideList : R.string.dashboardShowList;
-        listView.setVisibility(visibilityId);
-        findViewById(R.id.toggleDataRadio).setVisibility(visibilityId);
-        findViewById(R.id.noDataFoundLabel).setVisibility(visibilityId);
-        ((Button) v).setText(getString(buttonTextId));
-    }
+
 
     /**
      * Toggle totals.
@@ -206,42 +100,9 @@ public class DashboardActivity extends InternalActivity
         totals.put("balance", (TextView) findViewById(R.id.dashboardCurrentBalance));
         totals.put("income", (TextView) findViewById(R.id.totalIncomeView));
         totals.put("expenditure", (TextView) findViewById(R.id.totalExpenditureView));
-        listView = (ListView) findViewById(R.id.transactionList);
-        noDataFoundLabel = (TextView) findViewById(R.id.noDataFoundLabel);
-        ((RadioGroup) findViewById(R.id.toggleDataRadio)).setOnCheckedChangeListener((group, checkedId) -> updateView());
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            Transaction transaction = (Transaction) listView.getItemAtPosition(position);
-            Toast.makeText(DashboardActivity.this, transaction.toString(), Toast.LENGTH_SHORT).show();
-        });
         observer = new BalanceObserver(this, totals);
         userObserver = new UserDisplayNameObserver(this, ((TextView) findViewById(R.id.userNameLabel)));
     }
 
-    private void updateView()
-    {
-        List<Transaction> transactionList;
-        if (((RadioButton) findViewById(R.id.showAllRadioButton)).isChecked()) {
-            transactionList = transactionDataService.getTransactions();
-        } else if (((RadioButton) findViewById(R.id.showIncomeRadioButton)).isChecked()) {
-            transactionList = transactionDataService.getIncomes();
-        } else {
-            transactionList = transactionDataService.getExpenditures();
-        }
-        transactionAdapter = new TransactionAdapter(this, transactionList);
-        listView.setAdapter(transactionAdapter);
-        toggleUi(!(transactionList == null || transactionList.isEmpty()));
-    }
 
-    private void toggleUi(boolean hasData)
-    {
-        if (hasData) {
-            noDataFoundLabel.setVisibility(View.GONE);
-            if (listShown) {
-                listView.setVisibility(View.VISIBLE);
-            }
-            return;
-        }
-        noDataFoundLabel.setVisibility(View.VISIBLE);
-        listView.setVisibility(View.GONE);
-    }
 }
